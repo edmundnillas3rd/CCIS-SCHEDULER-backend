29adff4ba29cf712d7c137a9caddfa815dd5f375
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-04-07 07:59:31 +0800

    Replace database config connection

--- ccis_scheduler_server/config/db.js
+++ ccis_scheduler_server/config/db.js
@@ -2,13 +2,15 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 
-const db = mysql
-  .createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-  })
-  .promise();
+// const db = mysql
+//   .createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PWD,
+//     database: process.env.DB_NAME,
+//   })
+//   .promise();
+
+const db = mysql.createConnection(process.env.DB_URL).promise();
 
 module.exports = db;
