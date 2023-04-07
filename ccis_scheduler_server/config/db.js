655b40d7e199cfeaf720fdd60ccb41a85ba44909
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-04-07 14:25:30 +0800

    Add condition for handling different env modes

--- ccis_scheduler_server/config/db.js
+++ ccis_scheduler_server/config/db.js
@@ -2,15 +2,19 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 
-// const db = mysql
-//   .createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PWD,
-//     database: process.env.DB_NAME,
-//   })
-//   .promise();
+let db;
 
-const db = mysql.createConnection(process.env.DB_URL).promise();
+if (process.env.NODE_ENV !== "production") {
+  db = mysql
+    .createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PWD,
+      database: process.env.DB_NAME,
+    })
+    .promise();
+} else {
+  db = mysql.createConnection(process.env.DB_URL).promise();
+}
 
 module.exports = db;
